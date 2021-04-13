0595059eb83c2a5281e80bb6b45fef50bf85c632
Nastya Kvak <nastya.kvak@codeimmersives.com>
2021-04-13 11:59:00 -0400

    Adds sorting features

--- main.js
+++ main.js
@@ -19,26 +19,137 @@ const parent = document.createElement('div')
 const ol = document.createElement('ol')
 parent.appendChild(ol)
 
+const commentsButton = document.querySelector('#comments')
+commentsButton.addEventListener('click', ()=>{
+    const list = document.querySelectorAll('li')
+    for (const li of list){
+        li.remove(li)
+    }
+    fetch(top100)
+        .then((res)=>res.json())
+        .then((data)=>{
+            let promises = []
+            for (let i=0; i<100;i++){
+                let id = data[i].toString()
+                let link = article+ id +'.json?print=pretty'
+                const prom = fetch(link)
+                promises.push(prom)
+            }
+            const allPromises = Promise.all(promises)
+
+            allPromises 
+                .then(rawResponses=>{
+                    let jsonPromises = rawResponses.map(rawResponse => rawResponse.json())
+                    return Promise.all(jsonPromises)
+                })
+                .then(objects =>{
+                    objects.sort(function (a,b){
+                        return b.descendants-a.descendants
+                    })
+                    console.log(objects)
+                    for (let i=0;i<100;i++){
+                        const body = document.querySelector('#main')
+                        const a = document.createElement('a')
+                        const li = document.createElement('li')
+                        parent.className = 'parent'
+                        const child = document.createElement('div')
+                        child.className = 'child'
+                        child.innerText = objects[i].score + " score" + ' | Submitted by ' + objects[i].by + ' | ' + objects[i].descendants +' comments'
+                        a.href = objects[i].url
+                        a.innerText = objects[i].title
+                        li.appendChild(a)
+                        li.appendChild(child)
+                        ol.appendChild(li)
+                        body.appendChild(parent)
+                        a.style.textDecoration = 'none'
+                        a.target=target="_blank_"
+                        parent.style.fontSize = '20px'
+                        child.style.fontSize = '16px'
+                        child.style.color = 'grey'
+                        parent.style.marginLeft = '150px'
+                        parent.style.color = 'black'
+                        li.style.marginTop='20px'
+                        li.style.marginBottom='10px'
+                        ol.style.color='rgb(185,50,124)'
+                    }
+                })
+        })
+})
+
+
+const scoreButton = document.querySelector('#score')
+scoreButton.addEventListener('click', ()=>{
+    const list = document.querySelectorAll('li')
+    for (const li of list){
+        li.remove(li)
+    }
+    fetch(top100)
+        .then((res)=>res.json())
+        .then((data)=>{
+            let promises = []
+            for (let i=0; i<100;i++){
+                let id = data[i].toString()
+                let link = article+ id +'.json?print=pretty'
+                const prom = fetch(link)
+                promises.push(prom)
+            }
+            const allPromises = Promise.all(promises)
+
+            allPromises 
+                .then(rawResponses=>{
+                    let jsonPromises = rawResponses.map(rawResponse => rawResponse.json())
+                    return Promise.all(jsonPromises)
+                })
+                .then(objects =>{
+                    objects.sort(function (a,b){
+                        return b.score-a.score
+                    })
+                    console.log(objects)
+                    for (let i=0;i<100;i++){
+                        const body = document.querySelector('#main')
+                        const a = document.createElement('a')
+                        const li = document.createElement('li')
+                        parent.className = 'parent'
+                        const child = document.createElement('div')
+                        child.className = 'child'
+                        child.innerText = objects[i].score + " score" + ' | Submitted by ' + objects[i].by + ' | ' + objects[i].descendants +' comments'
+                        a.href = objects[i].url
+                        a.innerText = objects[i].title
+                        li.appendChild(a)
+                        li.appendChild(child)
+                        ol.appendChild(li)
+                        body.appendChild(parent)
+                        a.style.textDecoration = 'none'
+                        a.target=target="_blank_"
+                        parent.style.fontSize = '20px'
+                        child.style.fontSize = '16px'
+                        child.style.color = 'grey'
+                        parent.style.marginLeft = '150px'
+                        parent.style.color = 'black'
+                        li.style.marginTop='20px'
+                        li.style.marginBottom='10px'
+                        ol.style.color='rgb(185,50,124)'
+                    }
+                })
+        })
+})
 
 // function main(){
     fetch(top100)
         .then((res)=>res.json())
         .then((data)=>{
             for (let i=0; i<=100; i++){
-                console.log(data[i])
                 let id = data[i].toString()
                 let link = article+ id +'.json?print=pretty'
-                console.log(link)
                 
                 fetch(link)
                     .then ((result)=>result.json())
                     .then((story)=>{
                         if (data[i] === story.id){
-                            console.log(story.url)
                             const body = document.querySelector('#main')
                             const a = document.createElement('a')
                             const li = document.createElement('li')
-                            parent.className = 'child'
+                            parent.className = 'parent'
                             const child = document.createElement('div')
                             child.className = 'child'
                             child.innerText = story.score + " score" + ' | Submitted by ' + story.by + ' | ' + story.descendants +' comments'
@@ -49,6 +160,7 @@ parent.appendChild(ol)
                             ol.appendChild(li)
                             body.appendChild(parent)
                             a.style.textDecoration = 'none'
+                            a.target=target="_blank_"
                             parent.style.fontSize = '20px'
                             child.style.fontSize = '16px'
                             child.style.color = 'grey'
@@ -58,14 +170,8 @@ parent.appendChild(ol)
                             li.style.marginBottom='10px'
                             ol.style.color='rgb(185,50,124)'
                         }
-                        // const scoreButton = document.querySelector('#score')
-                        // scoreButton.addEventListener('click', ()=>{
-                            
-                        // }) 
                     })
                 }
-   
-            
 })
 
 
